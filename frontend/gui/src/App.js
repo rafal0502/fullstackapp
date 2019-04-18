6a318ea01fd462e5f3b7163693122e34bf6b8f88
rafal0502 <rafalchojnacki0502@gmail.com>
2019-04-18 09:52:06 -0700

    Modified content of layout

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import 'antd/dist/antd.css';
 import CustomLayout from './containers/Layout';
-import ArticleList from './containers/ArticleListView';
+import BaseRouter from './routes';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CustomLayout>
-          <ArticleList />
-        </CustomLayout>
+        <Router>
+          <CustomLayout>
+            <BaseRouter />
+          </CustomLayout>
+        </Router>
       </div>
     );
   }
--- frontend/gui/src/containers/Layout.js
+++ frontend/gui/src/containers/Layout.js
@@ -12,14 +12,14 @@ const CustomLayout = (props) => {
                     theme="dark"
                     mode="horizontal"
                     defaultSelectedKeys={['2']}
-                    style={{ lineHeight: '64px' }}
+                    style={{ lineHeight: '64px', textAlign: 'left' }}
                 >
                     <Menu.Item key="1">nav 1</Menu.Item>
                     <Menu.Item key="2">nav 2</Menu.Item>
                     <Menu.Item key="3">nav 3</Menu.Item>
                 </Menu>
             </Header>
-            <Content style={{ padding: '0 50px' }}>
+            <Content style={{ padding: '0 50px', textAlign: 'left' }}>
                 <Breadcrumb style={{ margin: '16px 0' }}>
                     <Breadcrumb.Item>Home</Breadcrumb.Item>
                     <Breadcrumb.Item>List</Breadcrumb.Item>
