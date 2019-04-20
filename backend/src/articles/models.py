16dac55d89eebf92c1085145c4df3cd949690064
rafal0502 <rafalchojnacki0502@gmail.com>
2019-04-20 03:39:47 -0700

    Added description to model

--- backend/src/articles/models.py
+++ backend/src/articles/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
+    description = models.TextField()
 
     def __str__(self):
         return self.title
